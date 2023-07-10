8af59d80e92403629a67c8ad7710f21fdb695841
Pradheep Krishna <padhupradheep@gmail.com>
2023-07-10 16:06:14 +0200

    Namespace handle (#23)
    * adding namespace handle for all the hardware components
    
    * expanding namespaces for configs
    
    * creating namespacing support for multi robot navigation
    
    * cd sr
    
    * update multi robot params
    
    * updates
    
    * final fix

--- configs/kinematics/kinematics.launch.py
+++ configs/kinematics/kinematics.launch.py
@@ -7,7 +7,14 @@ import launch_ros.actions
 
 def generate_launch_description():
     config = os.path.join(get_package_share_directory('neo_mp_400-2'),'configs/kinematics','kinematics.yaml')
+    robot_namespace = launch.substitutions.LaunchConfiguration('namespace', default="")
+
     return launch.LaunchDescription([
-        launch_ros.actions.Node(package='neo_kinematics_differential', executable='neo_differential_node', output='screen',
-            name='neo_differential_node', parameters = [config])
+        launch_ros.actions.Node(
+            package='neo_kinematics_differential',
+            executable='neo_differential_node',
+            output='screen',
+            namespace = robot_namespace,
+            name='neo_differential_node',
+            parameters = [config])
     ])
--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -7,47 +7,61 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, Exec
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
 from launch_ros.actions import Node
+from launch.launch_context import LaunchContext
 import os
 from pathlib import Path
 
 def generate_launch_description():
     neo_mp_400 = get_package_share_directory('neo_mp_400-2')
-
+    robot_namespace = LaunchConfiguration('robot_namespace', default='robot1')
     urdf = os.path.join(get_package_share_directory('neo_mp_400-2'), 'robot_model/mp_400', 'mp_400.urdf')
+    context = LaunchContext()
 
     with open(urdf, 'r') as infp:  
         robot_desc = infp.read() # Dummy to use parameter instead of using argument=[urdf] in Node. Reference page: https://github.com/ros2/demos/pull/426/commits/a35a25732159e4c8b5655755ce31ec4c3e6e7975
-
-    rsp_params = {'robot_description': robot_desc}
 
     start_robot_state_publisher_cmd = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
-        parameters=[rsp_params])
+        namespace=robot_namespace,
+        parameters=[{'robot_description': robot_desc, 'frame_prefix': robot_namespace}],
+        arguments=[urdf])
 
     relayboard = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(neo_mp_400, 'configs/relayboard_v2', 'relayboard_v2.launch.py')
-            )
+            ),
+            launch_arguments={
+                'namespace': robot_namespace
+            }.items()
         )
 
     kinematics = IncludeLaunchDescription(
              PythonLaunchDescriptionSource(
                  os.path.join(neo_mp_400, 'configs/kinematics', 'kinematics.launch.py')
-             )
+             ),
+            launch_arguments={
+                'namespace': robot_namespace
+            }.items()
          )
     teleop = IncludeLaunchDescription(
              PythonLaunchDescriptionSource(
                  os.path.join(neo_mp_400, 'configs/teleop', 'teleop.launch.py')
-             )
+             ),
+            launch_arguments={
+                'namespace': robot_namespace
+            }.items()
          )
 
     laser = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(neo_mp_400, 'configs/lidar/sick/s300', 'sick_s300.launch.py')
-            )
+             ),
+            launch_arguments={
+                'namespace': robot_namespace
+            }.items()
         )
 
 
