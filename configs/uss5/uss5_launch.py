ef2f02ab93033535872731d8cf2f599ee0b73dd7
Pradheep <padhupradheep@gmail.com>
2024-04-08 15:50:53 +0200

    updating the correct directory

--- configs/uss5/uss5_launch.py
+++ configs/uss5/uss5_launch.py
@@ -6,7 +6,7 @@ from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 
 def generate_launch_description():
-    config = os.path.join(get_package_share_directory('neo_mp_400-2'),'launch','uss5.yaml')
+    config = os.path.join(get_package_share_directory('neo_mp_400-2'),'configs','uss5/uss5.yaml')
     return launch.LaunchDescription([
         launch_ros.actions.Node(
             package='neo_usboard_v2', executable='neo_usboard_v2_node', output='screen', parameters = [config])
