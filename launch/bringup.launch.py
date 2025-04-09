1631fc15a19f84f1566f04c2fb62bc7baa762052
Adarsh Karan Kesavadas Prasanth <adarshkaran01@gmail.com>
2025-04-09 14:08:41 +0530

    Addressing PR #33 comments, updated navigation.yaml, adding docking condition, fixed indentation and formatting issues, updated dependencies

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -10,10 +10,10 @@ from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import (
-  DeclareLaunchArgument,
-  IncludeLaunchDescription,
-  OpaqueFunction
-)
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction
+    )
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
@@ -27,7 +27,7 @@ def execution_stage(context: LaunchContext,
                     d435_enable,
                     uss_enable,
                     scanner_type):
-    
+
     imu_enabl = str(imu_enable.perform(context))
     d435_enabl = str(d435_enable.perform(context))
     uss_enabl = str(uss_enable.perform(context))
@@ -44,7 +44,7 @@ def execution_stage(context: LaunchContext,
     urdf = os.path.join(neo_mp_400,
         'robot_model/mp_400',
         'mp_400.urdf.xacro')
-    
+
     # Start robot state publisher
     start_robot_state_publisher_cmd = Node(
         package='robot_state_publisher',
@@ -90,7 +90,7 @@ def execution_stage(context: LaunchContext,
             }.items(),
             condition=IfCondition(imu_enable)
         )
-    
+
     launches.append(imu)
 
     # 6. D435
@@ -125,17 +125,17 @@ def generate_launch_description():
             'robot_namespace', default_value='', 
             description='Top-level namespace'
         )
-    
+
     declare_imu_cmd = DeclareLaunchArgument(
             'imu_enable', default_value='False',
             description='Enable IMU - Options: True/False'
         )
-    
+
     declare_realsense_cmd = DeclareLaunchArgument(
             'd435_enable', default_value='False',
             description='Enable Realsense - Options: True/False'
         )
-    
+
     declare_uss_cmd = DeclareLaunchArgument(
             'uss_enable', default_value='False',
             description='Enable uss - Options: True/False'
@@ -146,7 +146,7 @@ def generate_launch_description():
             choices=['', 'sick_s300', 'sick_microscan3'],
             description='Type of laser scanner to use'
         )
-    
+
     #  Launch hardware nodes
     # 1. Relayboard
     relayboard = IncludeLaunchDescription(
@@ -177,7 +177,7 @@ def generate_launch_description():
                 'namespace': robot_namespace
             }.items()
         )
-    
+
     # Opaque function for configuring URDF, IMU, Realsense and the USBoard
     opq_function = OpaqueFunction(function=execution_stage, args=context_arguments)
 
--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -17,24 +17,31 @@ from launch_ros.actions import Node, PushRosNamespace
 from launch.conditions import IfCondition
 from launch.launch_context import LaunchContext
 
-
 def execution_stage(
         context: LaunchContext,
         use_sim_time,
         autostart, namespace, use_multi_robots,
-        head_robot, use_amcl, map_dir, param_dir, use_rviz):
-    
+        head_robot, use_amcl, map_dir, 
+        param_dir, use_rviz, docking):
+
     launches = []
 
     params = str(param_dir.perform(context))
+    enable_docking = str(docking.perform(context)).lower()
 
-    # If the parameter file is not provided, use the default one based on the robot type
+    # If the parameter file is not provided, use the default one based on the robot type and docking condition
     if not params:
-        params = os.path.join(
+        if enable_docking == "true":
+            params = os.path.join(
+                get_package_share_directory('neo_mp_400-2'),
+                'configs', 'navigation',
+                'docking_navigation.yaml')
+        else:
+            params = os.path.join(
                 get_package_share_directory('neo_mp_400-2'),
                 'configs', 'navigation',
                 'navigation.yaml')
-        
+
     nav2_launch_file_dir = os.path.join(get_package_share_directory('neo_nav2_bringup'), 'launch')
 
     # Start navigation and push namespace if and only if the multi robot scenario is set to true. 
@@ -50,8 +57,9 @@ def execution_stage(
                 'use_sim_time': use_sim_time,
                 'use_multi_robots': use_multi_robots,
                 'params_file': params,
-                'namespace': namespace}.items(),
+                'namespace': namespace}.items()
         ),
+
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([nav2_launch_file_dir, '/localization_amcl.launch.py']),
             condition=IfCondition(use_amcl),
@@ -60,16 +68,17 @@ def execution_stage(
                 'use_sim_time': use_sim_time,
                 'use_multi_robots': use_multi_robots,
                 'params_file': params,
-                'namespace': namespace}.items(),
+                'namespace': namespace}.items()
         ),
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([nav2_launch_file_dir, '/navigation_neo.launch.py']),
-            launch_arguments={'namespace': namespace,
-                              'use_sim_time': use_sim_time,
-                              'params_file': params,
-                              'use_rviz': use_rviz,
-                              }.items()),
+            launch_arguments={
+                'namespace': namespace,
+                'use_sim_time': use_sim_time,
+                'params_file': params,
+                'use_rviz': use_rviz}.items()
+        )
     ])
 
     # Start map_server if this robot is assigned as the head robot and if there is no multi-robot,
@@ -92,8 +101,9 @@ def execution_stage(
             name='lifecycle_manager_localization',
             output='screen',
             parameters=[{'use_sim_time': use_sim_time},
-						{'autostart': autostart},
-                        {'node_names': ['map_server']}])
+                        {'autostart': autostart},
+                        {'node_names': ['map_server']}]
+            )
         ]
     )
 
@@ -113,37 +123,38 @@ def generate_launch_description():
     map_dir = LaunchConfiguration('map')
     param_dir = LaunchConfiguration('nav2_params_file')
     use_rviz = LaunchConfiguration('use_rviz')
+    docking = LaunchConfiguration('enable_docking')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
             'use_sim_time', default_value='false',
             description='Use simulation clock if true'
         )
-    
+
     declare_autostart_cmd = DeclareLaunchArgument(
             'autostart', default_value='true',
             description='Automatically start the nav2 stack'
         )
-    
+
     declare_namespace_cmd = DeclareLaunchArgument(
             'robot_namespace', default_value='',
             description='Top-level namespace'
         )
-    
+
     declare_use_multi_robots_cmd = DeclareLaunchArgument(
             'use_multi_robots', default_value='False',
             description='Use multi robots'
         )
-    
+
     declare_head_robot_cmd = DeclareLaunchArgument(
             'head_robot', default_value='False',
             description='Head robot'
         )
-    
+
     declare_use_amcl_cmd = DeclareLaunchArgument(
             'use_amcl', default_value='False',
             description='Use AMCL'
         )
-    
+
     declare_map_cmd = DeclareLaunchArgument(
             'map', default_value=os.path.join(
                 get_package_share_directory('neo_mp_400-2'),
@@ -153,18 +164,23 @@ def generate_launch_description():
                 'neo_workshop.yaml'),
             description='Full path to map file to load'
         )
-    
+
     declare_nav2_param_file_cmd = DeclareLaunchArgument(
             'nav2_params_file', default_value="",
             description='Full path to the Nav2 parameters file to load.\n'
                         '\tLeave empty to use the default file based on the robot type'
         )
-    
+
     declare_use_rviz_cmd = DeclareLaunchArgument(
             'use_rviz', default_value='True',
             description='Launch RViz for visualization'
         )
-    
+
+    declare_docking_cmd = DeclareLaunchArgument(
+            'enable_docking', default_value='False',
+            description='Enable docking for robot'
+        )
+
     # Adding all the necessary launch description actions
     launch_desc.add_action(declare_use_sim_time_cmd)
     launch_desc.add_action(declare_autostart_cmd)
@@ -175,9 +191,10 @@ def generate_launch_description():
     launch_desc.add_action(declare_map_cmd)
     launch_desc.add_action(declare_nav2_param_file_cmd)
     launch_desc.add_action(declare_use_rviz_cmd)
+    launch_desc.add_action(declare_docking_cmd)
 
     context_arguments = [use_sim_time, autostart, namespace,
-                         use_multi_robots, head_robot, use_amcl, map_dir, param_dir, use_rviz]
+                         use_multi_robots, head_robot, use_amcl, map_dir, param_dir, use_rviz, docking]
 
     opq_function = OpaqueFunction(function=execution_stage, args=context_arguments)
 
