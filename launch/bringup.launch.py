ed91a5012739eaffe15162f70fa5869447b34057
Adarsh Karan Kesavadas Prasanth <112091438+AdarshKaran@users.noreply.github.com>
2025-06-10 16:57:02 +0530

    URDF parsing (#39)
    * fixing URDF parsing
    
    * removed joint name prefixes for wheels and castors
    
    * adding relay topic for joint_states
    
    * update slam  mapping launch file
    
    * Updated case for launch args default value
    
    * Added author names

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -17,7 +17,7 @@ from launch.actions import (
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
-
+from launch_ros.descriptions import ParameterValue
 from launch.launch_context import LaunchContext
 from launch.conditions import IfCondition
 
@@ -53,17 +53,17 @@ def execution_stage(context: LaunchContext,
         output='screen',
         namespace=robot_namespace,
         parameters=[{
-            'robot_description': Command([
+            'robot_description': ParameterValue(Command([
                 "xacro", " ", urdf,
                 " ", 'use_imu:=', imu_enabl,
                 " ", 'use_d435:=', d435_enabl,
                 " ", 'use_uss:=', uss_enabl,
                 " ", 'scanner_type:=', scanner_typ,
-            ]),
+            ]), value_type=str),
             'frame_prefix': rp_ns
         }],
         arguments=[urdf]
-        )
+    )
 
     launch_actions.append(start_robot_state_publisher_cmd)
 
@@ -105,6 +105,16 @@ def execution_stage(context: LaunchContext,
         )
 
     launch_actions.append(d435)
+
+    relay_topic_joint_states = Node(
+        package='topic_tools',
+        executable='relay',
+        name='relay_joint_states',
+        output='screen',
+        parameters=[{'input_topic': "drives/joint_states",'output_topic': "joint_states"}]
+    )
+
+    launch_actions.append(relay_topic_joint_states)
 
     return launch_actions
 
